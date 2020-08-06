18d780ffac728cf91b836d30d3629402aca134d6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-08-06 15:09:39 -0400

    Begin to move snake

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const score = document.querySelector("#score");
 let currentSnake = [2, 1, 0];
 let squares = [];
 let direction = 1;
+let width = 10;
 
 const createGrid = () => {
   for (let i = 0; i < 100; i++) {
@@ -24,6 +25,19 @@ const move = () => {
   currentSnake.unshift(currentSnake[0] + direction);
   squares[currentSnake[0]].classList.add("snake");
 };
-move();
 
 let gameLoop = setInterval(move, 1000);
+
+const snakeMovement = (e) => {
+  if (e.keyCode === 39) {
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    direction = +width;
+  }
+};
+
+document.addEventListener("keyup", snakeMovement);

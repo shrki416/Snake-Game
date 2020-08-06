79fb6c489fda282f92f9c1834eaf387700860ec3
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-08-06 15:36:24 -0400

    Create apple, change snake direction

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const grid = document.querySelector(".grid");
 const startButton = document.querySelector("#start");
 const score = document.querySelector("#score");
-let currentSnake = [2, 1, 0];
 let squares = [];
+let snake = [2, 1, 0];
+let apple = 0;
 let direction = 1;
-let width = 10;
+const width = 10;
 
 const createGrid = () => {
   for (let i = 0; i < 100; i++) {
@@ -17,16 +18,33 @@ const createGrid = () => {
 
 createGrid();
 
-currentSnake.forEach((index) => squares[index].classList.add("snake"));
+snake.forEach((index) => squares[index].classList.add("snake"));
 
 const move = () => {
-  const tail = currentSnake.pop();
+  if (
+    (snake[0] + width >= width * width && direction === width) ||
+    (snake[0] % width === width - 1 && direction === 1) ||
+    (snake[0] % width === 0 && direction === -1) ||
+    (snake[0] - width < 0 && direction === -width) ||
+    squares[snake[0] + direction].classList.contains("snake")
+  )
+    return clearInterval(gameLoop);
+
+  const tail = snake.pop();
   squares[tail].classList.remove("snake");
-  currentSnake.unshift(currentSnake[0] + direction);
-  squares[currentSnake[0]].classList.add("snake");
+  snake.unshift(snake[0] + direction);
+  squares[snake[0]].classList.add("snake");
 };
 
 let gameLoop = setInterval(move, 1000);
+
+const generateApple = () => {
+  do {
+    apple = Math.floor(Math.random() * squares.length);
+  } while (squares[apple].classList.contains("snake"));
+  squares[apple].classList.add("apple");
+};
+generateApple();
 
 const snakeMovement = (e) => {
   if (e.keyCode === 39) {

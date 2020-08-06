f13d7652ed33e36e92f9ee9eb5bd8f429d403a47
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-08-06 14:45:48 -0400

    Create a snake body

---
+++ script.js
@@ -0,0 +1,27 @@
+const grid = document.querySelector(".grid");
+const startButton = document.querySelector("#start");
+const score = document.querySelector("#score");
+let currentSnake = [2, 1, 0];
+let squares = [];
+let direction = 1;
+
+const createGrid = () => {
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+    squares.push(square);
+  }
+};
+
+createGrid();
+
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+const move = () => {
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+  squares[currentSnake[0]].classList.add("snake");
+};
+move();
